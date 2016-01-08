f4a3810314b79897365d7102eecb254e64ffb3a7
J. Andrade <ndrd@ciencias.unam.mx>
2016-01-08 02:38:42 -0600

    Agregando funcionalidad al mapa

--- lokator/www/js/record.js
+++ lokator/www/js/record.js
@@ -1,7 +1,31 @@
 function initMap() {
-	window.map_record = new google.maps.Map(document.getElementById('map_record'), {
+	var map = null;
+	window.map_record = map =  new google.maps.Map(document.getElementById('map_record'), {
 	   center: {lat: -34.397, lng: 150.644},
-	   zoom: 6
+	   zoom: 15,
+	   scrollwheel :false,
+	   disableDefaultUI: true
 	 });
+
+	showCurrentPosition(map);
 }
+
+function showCurrentPosition(map) {
+	var fgGeo = window.navigator.geolocation;
+
+	// Your app must execute AT LEAST ONE call for the current position via standard Cordova geolocation,
+	//  in order to prompt the user for Location permission.
+	fgGeo.getCurrentPosition(function(location) {
+	    var coords  = location.coords,
+	        ll      = new google.maps.LatLng(coords.latitude, coords.longitude),
+	        zoom    = map.getZoom();
+
+	    map.setCenter(ll);
+	    if (zoom < 15) {
+	        map.setZoom(15);
+	    }
+	});
+
+}
+
 
