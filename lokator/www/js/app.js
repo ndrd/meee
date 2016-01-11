98de9c728eee8e167192b195348be7598fe20890
J. Andrade <ndrd@ciencias.unam.mx>
2016-01-11 00:15:00 -0600

    It works negro

--- lokator/www/js/app.js
+++ lokator/www/js/app.js
@@ -1,49 +1,7 @@
-// var showHola = function () {
-// 	$("#title").text("кати");
-// };
-
-// var cb = function () {
-// 	var _main_map = Core.Map.init('map_record', true);
-
-// 	/* record buttons */
-// 	var $locate = $("#locate"),
-// 		$brdcst = $("#broadcast"),
-// 		$record = $("#record");
-
-// 	var $ss 	= $("#ss");
-	
-// 	 listeners 
-// 	$ss.on('focusout', (e) => {
-// 		alert('server set to: ', $(this).val());
-// 	});
-
-// 	$locate.on('click', (e) => {
-// 		_main_map.getCurrentPosition();
-// 	});
-
-// 	$brdcst.on('click', (e) => {
-// 		alert('emit to socket');
-// 	});
-
-// 	$record.on('click', (e) => {	
-// 		alert('saving track');
-// 	})
-// }
-
-
-$.noConflict();
+var app = {};
 
 function initMap() {
-	var map = null;
-	window.map_record = map =  new google.maps.Map(document.getElementById('map_record'), {
-	   center: {lat: -34.397, lng: 150.644},
-	   zoom: 5,
-	   scrollwheel :false,
-	   disableDefaultUI: true
-	 });
-	alert("negros");
-	alert(jQuery);
-
+	app.map = Core.Map.init('map_record', true);
 }
 
 document.addEventListener('deviceready', function () {
@@ -64,6 +22,29 @@ document.addEventListener('deviceready', function () {
 }, false);
 
 jQuery( document ).ready(function( $ ) {
-  // Code that uses jQuery's $ can follow here.
-  $("#title").text('negros');
+	var map = app.map;
+	/* record buttons */
+	var $locate = $("#locate"),
+		$brdcst = $("#broadcast"),
+		$record = $("#record");
+
+	var $ss 	= $("#ss");
+
+	//listeners 
+	$ss.on('focusout', function(e) {
+		alert('server set to: ', $(this).val());
+	});
+
+ 	$locate.on('click', function (e) {
+  		app.getCurrentPosition();
+  	});
+
+  	$brdcst.on('click', function (e) {
+  		alert('emit to socket');
+  	});
+
+  	$record.on('click', function (e) {	
+  		alert('saving track');
+  	})
+
 });
