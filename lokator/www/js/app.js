cbeed49ef3fa6361c3ff5bf39f2e953cd98d813e
J. Andrade <ndrd@ciencias.unam.mx>
2016-01-10 22:22:44 -0600

    Works on browser, not device

--- lokator/www/js/app.js
+++ lokator/www/js/app.js
@@ -1,4 +1,8 @@
-$(function() {
+var showHola = function () {
+	$("#title").text("кати");
+};
+
+var cb = function () {
 	var _main_map = Core.Map.init('map_record', true);
 
 	/* record buttons */
@@ -24,6 +28,25 @@ $(function() {
 	$record.on('click', (e) => {	
 		alert('saving track');
 	})
+}
+
+$(function() {
+	cb(); 
+
+
+});
+
+document.addEventListener('deviceready', function() {
+	if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+    }
 
 
 });
